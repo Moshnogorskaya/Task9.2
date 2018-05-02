d28fc4d08a3a892773bda3a1e6e1ed3f053e1ad3
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-05-02 17:16:59 +0300

    del doesnt work

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 let form = document.querySelector('.form');
+let container = document.querySelector('.container');
 let input = document.querySelector('.form__input');
 let warning = document.querySelector('.form__warning');
-input.oninput=function(){
-    warning.style.display='none';
+let output = document.querySelector('.form__output');
+let outputGroup = document.querySelector('.output-group');
+let del = document.querySelector('.form__delete-entry');
+input.oninput = () => {
+  warning.style.display = 'none';
+  input.value = input.value.replace(/</g, "&lt;");
 }
-form.onsubmit=function (){
-  let email=/^[\w\.\-]+@[\w\-]+\.[a-z]+$/;
-  if(input.value.search(email)==-1){
-      warning.style.display='flex';}
-
-      return false;
+form.onsubmit = () => {
+  let email = /^[\w\.\-]+@[\w\-]+\.[a-z]+$/;
+  if (input.value.search(email) == -1) {
+    warning.style.display = 'flex';
+  } else {
+    output.innerHTML = input.value;
+    input.value = '';
+    let dupOutput = outputGroup.cloneNode(true);
+    dupOutput.style.display = 'flex';
+    outputGroup.parentNode.insertBefore(dupOutput, outputGroup.parentNode.children[1]);
+  }
+  return false;
 }
-
+del.onclick = () => {
+    //container.removeChild(outputGroup);
+  return false;
+}
