74f1bb8ba6467515a0d7141b4dee772098cd8eff
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-05-02 18:00:00 +0300

    ready

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-let form = document.querySelector('.form');
-let container = document.querySelector('.container');
-let input = document.querySelector('.form__input');
-let warning = document.querySelector('.form__warning');
-let output = document.querySelector('.form__output');
-let outputGroup = document.querySelector('.output-group');
-let del = document.querySelector('.form__delete-entry');
+let form = document.querySelector(".form");
+let container = document.querySelector(".container");
+let input = document.querySelector(".form__input");
+let warning = document.querySelector(".form__warning");
+let output = document.querySelector(".form__output");
+let outputGroup = document.querySelector(".output-group");
 input.oninput = () => {
-  warning.style.display = 'none';
+  warning.style.display = "none";
   input.value = input.value.replace(/</g, "&lt;");
-}
+};
 form.onsubmit = () => {
   let email = /^[\w\.\-]+@[\w\-]+\.[a-z]+$/;
   if (input.value.search(email) == -1) {
-    warning.style.display = 'flex';
+    warning.style.display = "flex";
   } else {
     output.innerHTML = input.value;
-    input.value = '';
+    input.value = "";
     let dupOutput = outputGroup.cloneNode(true);
-    dupOutput.style.display = 'flex';
-    outputGroup.parentNode.insertBefore(dupOutput, outputGroup.parentNode.children[1]);
+    dupOutput.style.display = "flex";
+    outputGroup.parentNode.insertBefore(
+      dupOutput,
+      outputGroup.parentNode.children[1]
+    );
+    let del = document.querySelector(".form__delete-entry");
+    del.onclick = function() {
+      container.removeChild(del.parentNode);
+      return false;
+    };
   }
   return false;
-}
-del.onclick = () => {
-    //container.removeChild(outputGroup);
-  return false;
-}
+};
